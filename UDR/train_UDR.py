beb35955759b6a816bb31d7af638fa7cef3335a8
GayaCocca <gaya.coccagc@gmail.com>
2024-07-13 23:05:10 +0200

    Update train_UDR.py

--- UDR/train_UDR.py
+++ UDR/train_UDR.py
@@ -47,7 +47,7 @@ def main():
 
     # Evaluate the final model	
         for step in range(eval_interval, total_timesteps + 1, eval_interval):
-            model.learn(total_timesteps= lenss, reset_num_timesteps=False)
+            model.learn(total_timesteps= eval_interval, reset_num_timesteps=False)
             print('second loop')
             mean_reward, _ = evaluate_policy(model, test_env, n_eval_episodes=50, render=False)
             source_rewards[step].append(mean_reward)
